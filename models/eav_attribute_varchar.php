b2c03451c1a12660b2df83baa57be12e237c1ff9
Craig Morris <craig.michael.morris@gmail.com>
2009-05-14 10:38:34 +1000

    Added after find condition to varchar to convert file paths to web friendly (stupid windows) TODO: Put URLs and file types into their own table so that normal varchars arent affected. Removed EAV helper as this is not necessary

--- models/eav_attribute_varchar.php
+++ models/eav_attribute_varchar.php
@@ -3,6 +3,14 @@
 
 		var $fileTypes = array('image', 'flash', 'file');
 
+		/**
+		* Attaches an upload behavior to save file uploads automatically.
+		*
+		* @param mixed $data
+		* @param mixed $validate
+		* @param mixed $fieldList
+		* @return array
+		*/
 		function save($data = null, $validate = true, $fieldList = array()) {
 			// find the attribute for this varchar and determine if its a file upload
 			$conditions = array(
@@ -19,5 +27,21 @@
 			$return = parent::save($data, $validate, $fieldList);
 			return $return;
 		}
+
+		/**
+		* Modifys paths to files so they are web friendly.
+		*
+		* @param mixed $results
+		* @return mixed
+		*/
+		function afterFind($results) {
+			foreach ($results as &$result) {
+				if ( isset($result['EavAttributeVarchar']['value']) ) {
+					$result['EavAttributeVarchar']['value'] = str_replace('\\', '/', $result['EavAttributeVarchar']['value']);
+				}
+			}
+			return $results;
+		}
+
 	}
 ?>
