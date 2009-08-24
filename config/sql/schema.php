881a91a9fe993c859beed2435c159b5d72d880a2
Craig Morris <craig.michael.morris@gmail.com>
2009-08-24 22:57:18 +1000

    Adding schema in.

---
+++ config/sql/schema.php
@@ -0,0 +1,89 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* App schema generated on: 2009-08-24 22:08:43 : 1251117403*/
+class AppSchema extends CakeSchema {
+	var $name = 'App';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+	var $eav_attribute_booleans = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'attribute_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'model' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50),
+		'value' => array('type' => 'boolean', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
+	var $eav_attribute_datetimes = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'attribute_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'model' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50),
+		'value' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
+	var $eav_attribute_numbers = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'attribute_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'model' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50),
+		'value' => array('type' => 'float', 'null' => true, 'default' => NULL, 'length' => '9,3'),
+		'dir' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'mimetype' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50),
+		'filesize' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
+	var $eav_attribute_texts = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'attribute_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'model' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50),
+		'value' => array('type' => 'text', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
+	var $eav_attribute_varchars = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'attribute_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'model' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50),
+		'value' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
+	var $eav_attribute_files = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'attribute_id' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => NULL),
+		'model' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50),
+		'value' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
+	var $eav_attributes = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 100),
+		'model' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 50),
+		'type' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 20),
+		'options' => array('type' => 'string', 'null' => true, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1))
+	);
+
+}
+?>
